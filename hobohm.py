671b771ddef7760cd2098989651ead3a6a932989
Anders Gorm Pedersen <agpe@dtu.dk>
2022-09-19 13:21:51 +0200

    Rename variables

--- hobohm.py
+++ hobohm.py
@@ -8,11 +8,11 @@ from operator import itemgetter
 def main():
     parser = build_parser()
     args = parse_commandline(parser)
-    neighbordict = build_neighbordict(args)
+    neighbors = parse_neighbor_info(args)
     if args.keepfile:
-        neighbordict = handle_keeplist(args, neighbordict)
-    neighbor_count_dict = remove_neighbors(neighbordict)
-    keepnames = reinstate_neighborless_skipped(neighbordict, neighbor_count_dict)
+        neighbors = handle_keeplist(args, neighbors)
+    neighbor_count = remove_neighbors(neighbors)
+    keepnames = reinstate_neighborless_skipped(neighbors, neighbor_count)
     print_keepnames(keepnames)
 
 ################################################################################################
@@ -56,10 +56,10 @@ def parse_commandline(parser):
 
 ################################################################################################
 
-def build_neighbordict(args):
+def parse_neighbor_info(args):
 
-    # neighbordict should, for each item, contain a set of its neighbors (possibly empty)
-    neighbordict = defaultdict(set)
+    # neighbors should, for each item, contain a set of its neighbors (possibly empty)
+    neighbors = defaultdict(set)
 
     cutoff = args.cutoff    # Micro optimization: save time looking up dotted attributes
     values_are_sim = args.values_are_sim
@@ -74,93 +74,94 @@ def build_neighbordict(args):
 
                 if values_are_sim:
                     if value > cutoff:
-                        neighbordict[name1].add(name2)
-                        neighbordict[name2].add(name1)
+                        neighbors[name1].add(name2)
+                        neighbors[name2].add(name1)
                 else:
                     if value < cutoff:
-                        neighbordict[name1].add(name2)
-                        neighbordict[name2].add(name1)
+                        neighbors[name1].add(name2)
+                        neighbors[name2].add(name1)
 
-    return neighbordict
+    return neighbors
 
 ################################################################################################
 
-def handle_keeplist(args, neighbordict):
+def handle_keeplist(args, neighbors):
 
     # Read list of names to keep
     keepset = set()
     with open(args.keepfile, "r") as infile:
         for line in infile:
-            words = line.split()
-            keepset.add(words[0])
+            word = line.rstrip()
+            if word:                    # Maybe overkill to allow blank lines...
+                keepset.add(word)
 
     # First, check if any pair of keepset members are neighbors.
     # If so, print warning on stderr and artificially hide this fact
     for keepname1 in keepset:
         for keepname2 in (keepset - set([keepname1])):
-            if keepname2 in neighbordict[keepname1]:
+            if keepname2 in neighbors[keepname1]:
                 sys.stderr.write("# Keeplist warning: {} and {} are neighbors!".format(keepname1, keepname2))
-                neighbordict[keepname1].remove(keepname2)
-                neighbordict[keepname2].remove(keepname1)
+                neighbors[keepname1].remove(keepname2)
+                neighbors[keepname2].remove(keepname1)
 
     # Then, remove all neighbors of keepset members
     for keepname in keepset:
-        keepname_neighbors = list(neighbordict[keepname]) # To avoid issues with changing set during iteration
+        keepname_neighbors = list(neighbors[keepname]) # To avoid issues with changing set during iteration
 
         for remseq in keepname_neighbors:
             # Remove any mention of remseq in other entries
-            remseq_neighbors = list(neighbordict[remseq])
+            remseq_neighbors = list(neighbors[remseq])
             for remseq_neighbor in remseq_neighbors:
-                neighbordict[remseq_neighbor].remove(remseq)
+                neighbors[remseq_neighbor].remove(remseq)
 
             # Now remove remseq entry itself
-            del neighbordict[remseq]
+            del neighbors[remseq]
 
-    return(neighbordict)
+    return(neighbors)
 
 ################################################################################################
 
-def remove_neighbors(neighbordict):
+def remove_neighbors(neighbors):
 
     # Build dictionary keeping track of how many neighbors each item has
-    neighbor_count_dict = {}
-    for name in neighbordict:
-        neighbor_count_dict[name] = len(neighbordict[name])
+    neighbor_count = {}
+    for name in neighbors:
+        neighbor_count[name] = len(neighbors[name])
 
     # Find max number of neighbors and corresponding name
-    item_with_most_nb, max_num_nb = max(neighbor_count_dict.items(), key=itemgetter(1))
+    item_with_most_nb, max_num_nb = max(neighbor_count.items(), key=itemgetter(1))
 
     # While some items still have neighbors: remove an item with most neighbors, update counts
     # Note: could ties be dealt with intelligently?
     while max_num_nb > 0:
 
-        del(neighbor_count_dict[item_with_most_nb])
+        del(neighbor_count[item_with_most_nb])
 
         # Update neighbor counts
-        for item in neighbordict[item_with_most_nb]:
-            if item in neighbor_count_dict:
-                neighbor_count_dict[item] -= 1
+        for item in neighbors[item_with_most_nb]:
+            if item in neighbor_count:
+                neighbor_count[item] -= 1
 
         # Find new maximum number of neighbors
-        item_with_most_nb, max_num_nb = max(neighbor_count_dict.items(), key=itemgetter(1))
+        item_with_most_nb, max_num_nb = max(neighbor_count.items(), key=itemgetter(1))
 
-    return neighbor_count_dict
+    return neighbor_count
 
 ################################################################################################
 
-def reinstate_neighborless_skipped(neighbordict, neighbor_count_dict):
+def reinstate_neighborless_skipped(neighbors, neighbor_count):
 
     # Postprocess: reinstate skipped sequences that now have no neighbors
     # (This can happpen when ...?)
     # Note: order may have effect. Could this be optimized?
 
-    allseqs=set(neighbordict.keys())
-    keepseqs=set(neighbor_count_dict.keys())
+    allseqs=set(neighbors.keys())
+    keepseqs=set(neighbor_count.keys())
     skipseqs=allseqs - keepseqs
 
     for skipped in skipseqs:
         # if skipped sequence has no neighbors in keeplist
-        if not (neighbordict[skipped] & keepseqs):
+        if not (neighbors[skipped] & keepseqs):
             keepseqs.add(skipped)
 
     return keepseqs
@@ -168,7 +169,6 @@ def reinstate_neighborless_skipped(neighbordict, neighbor_count_dict):
 ################################################################################################
 
 def print_keepnames(keepnames):
-    # Print retained items
     for name in keepnames:
         print(name)
 
